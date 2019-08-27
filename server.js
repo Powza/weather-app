3f510525f9863ea2858352df53dbd24aaeaa7588
Derek <derek@tinyminute.com>
2019-08-26 20:31:51 -0400

    Update package

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-//const { createServer } = require("https");
-//const { parse } = require("url");
-//const { readFileSync } = require("fs");
 const express = require("express");
 const next = require("next");
 
@@ -8,11 +5,6 @@ const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
-
-/*const httpsOptions = {
-  key: readFileSync("./security/cert.key"),
-  cert: readFileSync("./security/cert.pem")
-};*/
 
 app
   .prepare()
@@ -23,17 +15,6 @@ app
       return handle(req, res);
     });
 
-    /*createServer(
-      httpsOptions,
-      (req, res) => {
-        const parsedUrl = parse(req.url, true);
-        handle(req, res, parsedUrl);
-      },
-      server
-    ).listen(port, err => {
-      if (err) throw err;
-      console.log(`> Ready on https://localhost:${port}`);
-    });*/
     server.listen(port, "0.0.0.0", err => {
       if (err) throw err;
       console.log(`> Ready on http://localhost:${port}`);
